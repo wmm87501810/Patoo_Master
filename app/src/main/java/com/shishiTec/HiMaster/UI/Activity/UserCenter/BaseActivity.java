24bf903d011c456d4ba00048fe31e97ddea535fa
wmm <272989027@qq.com>
2017-11-22 15:57:54 +0800

    新增BaseActivity

---
+++ app/src/main/java/com/shishiTec/HiMaster/UI/Activity/UserCenter/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.shishiTec.HiMaster.UI.Activity.UserCenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
